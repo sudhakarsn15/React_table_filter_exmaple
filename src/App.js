92d5bca0400109755f4b997cd13d62d3f3ce1eeb
sudhakarsn15 <sudhakarsn15@gmail.com>
2023-02-07 06:56:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./styles.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
-// import Table from "./Table";
+import Table from "./Table";
+import { useMemo } from "react/cjs/react.production.min";
 // import "./App.css";
 
 export default function App() {
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name",
+          },
+          {
+            Header: "Type",
+            accessor: "show.type",
+          },
+        ],
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language",
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres",
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime",
+          },
+          {
+            Header: "Status",
+            accessor: "show.status",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
   // data state to store the TV Maze API data. Its initial value is an empty array
   const [data, setData] = useState([]);
 
@@ -20,6 +69,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>React table component guide </h1>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
